a6c61dee63a1cab5c415071405de440176a5de63
Aleksei Sankov <belfigor333@gmail.com>
2017-12-20 15:53:16 +0300

    Implement server side

---
+++ server/src/BusinessLogic/Interfaces/ICommonInterface.cs
@@ -0,0 +1,21 @@
+﻿namespace MCS.BusinessLogic
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using MCS.DataAccess;
+    using System;
+
+    public interface ICommonInterface<TModel, in TDataContract> where TModel : BaseModel
+    {
+        Task<IEnumerable<TModel>> GetAsync();
+
+        Task<RequestResult<TModel>> GetAsync(string id);
+
+        Task<RequestResult<TModel>> PostAsync(TDataContract userData, Guid userId);
+
+        Task<RequestResult<TModel>> PutAsync(string id, TDataContract userData, Guid userId);
+
+        Task<RequestResult<TModel>> DeleteAsync(string id);
+    }
+}
---
+++ server/src/DataAccess/Models/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MCS.DataAccess
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Base model class with identifier.
+    /// </summary>
+    public abstract class BaseModel
+    {
+        /// <summary>
+        /// Object identifier.
+        /// </summary>
+        [Required]
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
